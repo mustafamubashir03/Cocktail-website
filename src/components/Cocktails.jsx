ce5102de2cdf2960cc3b80d87c11641b9dbfe3d0
Mustafa <mustafamubashir87@gmail.com>
2026-01-29 12:56:31 +0500

    Added Coctails section

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,79 @@
+import React, { useRef } from 'react'
+import { cocktailLists, mockTailLists } from '../../constant'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap/all'
+
+const Cocktails = () => {
+    const cocktailSecRef = useRef()
+    const leftLeafRef = useRef()
+    const rightLeafRef = useRef()
+    useGSAP(()=>{
+        const parallexTimeline = gsap.timeline({
+            scrollTrigger:{
+                trigger:cocktailSecRef.current,
+                markers:true,
+                start:'top 30%',
+                end:'bottom 80%',
+                scrub:true
+
+            }
+        })
+        parallexTimeline.from(leftLeafRef.current,{
+            x:-100,
+            y:100
+        }
+        ).from(rightLeafRef.current,{
+            x:100,
+            y:100
+        }
+        )
+
+    },[])
+
+  return (
+    <section ref={cocktailSecRef} id='cocktails' className='mb-40'>
+        <div className="noisy pointer-events-none z-0"/>
+
+        <img ref={leftLeafRef} src='/images/cocktail-left-leaf.png' alt='l-leaf' id='c-left-leaf'/>
+        <img ref={rightLeafRef} src='/images/cocktail-right-leaf.png' alt='r-leaf' id='c-right-leaf'/>
+
+        <div className='list'>
+            <div className='popular'>
+                <h2>Most popular cocktails:</h2>
+                <ul>
+                    {cocktailLists.map(({name,country,detail,price})=>{
+                        return (<li key={name}>
+                            <div className='md:me-28'>
+                                <h3>{name}</h3>
+                                <p>{country} | {detail}</p>
+                            </div>
+                            <span>- {price}</span>
+                        </li>)
+                    })}
+
+                </ul>
+
+            </div>
+            <div className='loved'>
+                <h2>Most loved cocktails:</h2>
+                <ul>
+                    {mockTailLists.map(({name,country,detail,price})=>{
+                        return <li key={name}>
+                            <div className='me-28'>
+                                <h3>{name}</h3>
+                                <p>{country} | {detail}</p>
+                            </div>
+                            <span>- {price}</span>
+                        </li>
+                    })}
+
+                </ul>
+
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Cocktails
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth:767})
     useGSAP(()=>{
         const heroSplit = new SplitText('.title',{type:'chars, words'});
         const paragraphSplit = new SplitText('.subtitle',{type:'lines'});
@@ -38,10 +41,29 @@ const Hero = () => {
             y:-200
         },0)
 
+        const startValue = isMobile ? 'top 50%': "center 60%";
+        const endValue = isMobile ? "120% top": "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger:'video',  
+                start:startValue,
+                end:endValue,
+                scrub:true,
+                pin:true
+                
+            }
+        })
+        videoRef.current.onloadedmetadata = ()=>{
+            tl.to(videoRef.current,{
+                currentTime:videoRef.current.duration
+            })
+        }
+
     },[])
   return (
     <>
-    <section id='hero' className='noisy'>
+    <section id='hero' className='noisy border-2'>
         <h1 className='title'>MOCTAIL</h1>
         <img src="/images/hero-left-leaf.png" alt="left-leaf" 
         className='left-leaf'/>
@@ -65,6 +87,9 @@ const Hero = () => {
  
         </div>
     </section>
+    <div className='video absolute inset-0'>
+        <video ref={videoRef} src='/videos/output.mp4' muted playsInline preload='auto'/>
+    </div>
     </>
   )
 }
